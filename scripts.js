5915e51b931eff2d37e48c437299f54d1a90ec84
Bhadra R <117386217+Bhadra2005@users.noreply.github.com>
2025-01-26 03:20:03 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-
 document.getElementById("login-button").addEventListener("click", function () {
     window.location.href = "authent.html";
+});
+
+document.getElementById('user-stats').addEventListener('click', function() {
+    window.location.href = 'userStats.html';
 });
