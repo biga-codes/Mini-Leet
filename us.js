862f1628c9e8322a7ecd8baec18716c7eead44b4
Bhadra R <117386217+Bhadra2005@users.noreply.github.com>
2025-01-26 03:31:33 +0530

    Update us.js

--- us.js
+++ us.js
@@ -2,20 +2,15 @@ document.getElementById('fetchStats').addEventListener('click', () => {
     const username = document.getElementById('username').value.trim();
   
     if (username) {
-      // Send the username to the background script
       chrome.runtime.sendMessage({ type: 'fetchStats', username: username });
     } else {
       document.getElementById('totalSolved').textContent = 'Please enter a username.';
     }
   });
   
-  // Listen for the response from the background script
   chrome.runtime.onMessage.addListener((message) => {
     if (message.type === 'userStats') {
-      // Display total solved problems
       document.getElementById('totalSolved').textContent = `Total Problems Solved: ${message.totalSolved}`;
-  
-      // Display solved problems by difficulty
       document.getElementById('easySolved').textContent = `Easy: ${message.easySolved}`;
       document.getElementById('mediumSolved').textContent = `Medium: ${message.mediumSolved}`;
       document.getElementById('hardSolved').textContent = `Hard: ${message.hardSolved}`;
